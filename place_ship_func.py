ff3dc2c9822b5bab52551a8c9ded2b38215e876c
szabadon <zsoltiusz.nagy@gmail.com>
2017-03-28 23:06:38 +0200

    put a lot of comments in place_ship_func to aid me tomorrow

--- place_ship_func.py
+++ place_ship_func.py
@@ -13,11 +13,11 @@ def place_ship(table, ship_dict, player):
     blank_page(player)
     print('#' * 80, '\nThis is the ship placement phase for {}!\n'.format(player), '#' * 79, '\n')
     reset_ship_dict()
-    current_ship = ''
+    current_ship = ''  # both of em are basically None
     ship_length = 0
-    ships_left = 2  # 7
+    ships_left = 2  # 7 is original value, this should be maybe like a global value, so it would be easier to change
     draw_table(table)
-    while ships_left > 0:
+    while ships_left > 0:  # this is biggest while-loop I ever seen, mobydick
         print("Ships you can place: ", ships_left)
         for ship in ship_dict:
             if ship_dict[ship][1] > 0:
@@ -27,28 +27,32 @@ def place_ship(table, ship_dict, player):
                 current_ship = ship
                 ship_length = ship_dict[ship][0]
                 ship_dict[ship][1] -= 1
-                break
+                break  # https://www.youtube.com/watch?v=qSVJnvWd1K0
 
-        let, num = target(ship_length, table)
-        options = placement_options_check(let, num, ship_length, table)
-        dir_list = []
-        direction = ''
+        let, num = target(ship_length, table)  # we place the first dot here
+        options = placement_options_check(let, num, ship_length, table)  # we check for options left and get a über-list
+        dir_list = []  # we will fill this list with option names (like 'UP', 'DOWN', etc)
+        direction = ''  # we will ask the user to input something to this string
 
-        print('Which direction do you want to place your {}?\n'.format(current_ship))
-        for i in options:
-            dir_list.append(i[0])
-            print(i[0], '\n')
+        print('Which direction do you want to place your {}?\n'.format(current_ship))  # so we ask
+        for i in options:  # we iterate through the options über-list we just got
+            dir_list.append(i[0])  # we fill our empty list with valid input options
+            print(i[0], '\n')  # then we print them out one-by-one on a new line to guide the user
 
-        while direction not in dir_list:
-            direction = input('> ')
+        while direction not in dir_list:  # till we get the right input
+            direction = input('> ')  # we prompt the user for one
 
-        for i in options:
-            if direction == i[0]:
+        for i in options:  # we iterate through our über-list again
+            if direction == i[0]:  # we double check something we made sure before(!?)
+                # here we check what is the input and draw a lot of X to that direction as long as ship-length(x)
+                # its redundant as hell of course
+                # a clever for-loop would be nice
+                # though maybe we have to redesign this whole process
                 # UP
                 if direction == 'UP':
                     for j in range(i[3], i[2]):
                         table[j][i[1]] = 'X'
-                    draw_table(table)
+                    draw_table(table)  # I think it's enough to put this line once on the end of this mess
                 # DOWN
                 if direction == 'DOWN':
                     for j in range(i[2], i[3]):
@@ -70,13 +74,14 @@ def place_ship(table, ship_dict, player):
 
 
 def placement_options_check(let, num, ship_length, table):
-    x = ship_length - 1
+    x = ship_length - 1  # why? maybe cause we already place an X in target() ...
     options = []
 
+    # this is redundant as hell
     # UP
-    up = False
-    s = num
-    e = num - x
+    up = False  # an empty variable is False too
+    s = num  # up-down duplicate
+    e = num - x  # also we will need better variable names
     if e >= 0:
         up = True
     for i in range(e, s):
@@ -130,25 +135,24 @@ def placement_options_check(let, num, ship_length, table):
     return options
 
 
-def reset_ship_dict():
+def reset_ship_dict():  # do we really need a function for this?
     ship_dict["Destroyer"][1] = 3
     ship_dict["Carrier"][1] = 1
     ship_dict["Battleship"][1] = 1
     ship_dict["Cruiser"][1] = 2
 
 
-def target(ship_length, table):
-    global abc
-    while True:
-        print('Choose your starting coordinates!\n')
-        let = ''
+def target(ship_length, table):  # we will need a more accurate name for this one
+    while True:  # we need to do this to let the user make mistakes
+        print('Choose your starting coordinates!\n')  # we need a more user-friendly solution for this
+        let = ''  # let my hand rot if I name a variable like this again
         while let not in abc:
             let = input('Vertical position (A-J): ')
 
         num = ''
         while num not in range(1, 11):
             num = input('Horizontal position (1-10): ')
-            try:
+            try:  # this and the while-loop its in just do the same, right?
                 num = int(num)
             except ValueError:
                 pass
@@ -156,12 +160,11 @@ def target(ship_length, table):
         let = int(abc.index(let))
         num -= 1
 
-        options = placement_options_check(let, num, ship_length, table)
+        options = placement_options_check(let, num, ship_length, table)  # here we do a doblue check
 
-        if table[num][let] != 'X' and len(options) >= 1:
+        if table[num][let] != 'X' and len(options) >= 1:  # thats actually a pretty one
             table[num][let] = 'X'
             draw_table(table)
             return let, num
-
         else:
             print('\nThere is not enough space for this ship...\n')
